7bed97cd6aa2d5cd16c69c4429beedcd76f460e8
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-30 09:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+class Driver {
+  constructor (name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf (year) {
+    return year - this.startDate.getFullYear();
+  }
+}
 
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  blocksTravelled() {
+    debugger;
+    return this.endingLocation.vertical - this.beginningLocation.vertical;
+  }
+}
+
+class Location {
+  constructor (horizontal, vertical) {
+    this.horizontal = horizontal;
+    this.vertical = vertical;
+  }
+}
